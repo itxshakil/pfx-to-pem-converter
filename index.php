b95274ef44e2870e2d5b21c3c21683ce77f8a21a
Shakil Alam <itxshakil@gmail.com>
2025-04-19 13:12:56 +0530

    chore: Remove duplicate

--- index.php
+++ index.php
@@ -92,9 +92,6 @@ if (empty($_SESSION['csrf_token'])) {
     <link href="/css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
